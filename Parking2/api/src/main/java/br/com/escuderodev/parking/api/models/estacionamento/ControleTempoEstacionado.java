770363c25a5f23ad4a4c349250805a956b7d650b
EDUARDO ESCUDERO <88359144+escuderodev@users.noreply.github.com>
2024-01-01 19:59:10 -0300

    att

--- Parking2/api/src/main/java/br/com/escuderodev/parking/api/models/estacionamento/ControleTempoEstacionado.java
+++ Parking2/api/src/main/java/br/com/escuderodev/parking/api/models/estacionamento/ControleTempoEstacionado.java
@@ -1,5 +1,20 @@
 package br.com.escuderodev.parking.api.models.estacionamento;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class ControleTempoEstacionado {
 
+//    public Long getTempoDeUtilizacao(LocalDateTime horaEntrada) {
+//        if (horaEntrada != null) {
+//            LocalDateTime horaSaida = LocalDateTime.now();
+//            Duration tempoEstacionado = Duration.between(horaEntrada,horaSaida);
+//            Long tempoDeUtilizacao = tempoEstacionado.toHours();
+//            System.out.println("Saida registrada em " + horaSaida);
+//            System.out.println("Tempo total estacionado: " + tempoDeUtilizacao + " horas.");
+//            return tempoDeUtilizacao;
+//        } else {
+//            System.out.println("Erro: registro de entrada não encontrado!");
+//        }
+//    }
 }
